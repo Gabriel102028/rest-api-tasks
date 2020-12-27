514b1132f25fd6ba71de5bf03c7e6428dffd5560
Gabriel Macias <gabriel_pedernera@hotmail.com.ar>
2020-12-27 12:22:58 -0300

    add valitade/pagination

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,9 +1,21 @@
-import Task from '../models/Task'
+import Task from '../models/Task';
+import { getPagination } from '../libs/getPagination';
 
 export const findALLTaks = async (req, res) => {
     try {
-        const tasks = await Task.find()
-        res.json(tasks)
+        const { size, page, title } = req.query;
+
+        const condition = title ? { title: { $regex: new RegExp(title), $options: "i" }, } : {};
+
+        const { limit, offset } = getPagination(page, size);
+
+        const data = await Task.paginate(condition, { offset, limit });
+        res.json({
+            totalItems: data.totalDocs,
+            tasks: data.docs,
+            totalPages: data.totalPages,
+            currentPage: data.page - 1
+        });
     } catch (error) {
         res.status(500).json({
             message: error.message || 'something goes wrong retrieving the tasks'
@@ -12,7 +24,11 @@ export const findALLTaks = async (req, res) => {
 };
 
 export const createTask = async (req, res) => {
-    
+
+    if (!req.body.title) {
+        return res.status(400).send({ message: 'Content cannot be empty' });
+    }
+
     try {
         const newTask = new Task({
             title: req.body.title,
@@ -29,15 +45,33 @@ export const createTask = async (req, res) => {
 }
 
 export const findOneTask = async (req, res) => {
-    const task = await Task.findById(req.params.id)
-    res.json(task)
+    const { id } = req.params;
+    try {
+        const task = await Task.findById(id);
+
+        if (!task) {
+            return res.status(400).json({ message: `this task with id ${id} does not exists` });
+        }
+        res.json(task);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || `Error retrieving task with id ${id}`
+        });
+    }
 }
 
 export const deleteTask = async (req, res) => {
-    await Task.findByIdAndDelete(req.params.id)
-    res.json({
-        message: 'task were deleted sucessfully'
-    })
+    const { id } = req.params;
+    try {
+        await Task.findByIdAndDelete(id);
+        res.json({
+            message: 'task were deleted sucessfully'
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || `Error to delete this task ${id}`
+        });
+    }
 }
 
 export const findAllDoneTasks = async (req, res) => {
